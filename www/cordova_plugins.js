f3cf6d0fe886d311148e4746435b9e443d26b74c
Rozaq Isnaini Nugraha <at.ozkula@gmail.com>
2017-02-06 15:36:32 +0700

    First

---
+++ www/cordova_plugins.js
@@ -0,0 +1,13 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+   
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-whitelist": "1.2.2",
+    "cordova-plugin-console": "1.0.5",
+    "cordova-plugin-statusbar": "1.0.1"
+}
+// BOTTOM OF METADATA
+});
